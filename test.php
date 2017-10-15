fa4ae4f7dca44d9b609024fd8fec621036c86b55
cD21 <minzeven@gmail.com>
2017-10-15 22:38:35 +0200

    rito pls

--- test.php
+++ test.php
@@ -1,21 +1,75 @@
 <?php
 
+$targetSummonerName = "Cichori";
+
 $config = json_decode(file_get_contents('config.json'), true);
+$summoner = apiCall('/lol/summoner/v3/summoners/by-name/'.$targetSummonerName);
+$matchList = apiCall('/lol/match/v3/matchlists/by-account/'.$summoner['accountId'].'/?queue=420&queue=440&endIndex=5');
 
-$result = apiCall('/lol/summoner/v3/summoners/by-name/Cichori');
+foreach ($matchList['matches'] as $match)
+{    
+    echo '<hr>match'.$match['gameId'];
+
+    $matchParticipantData = array();
+    $matchData = apiCall('/lol/match/v3/matches/'.$match['gameId']);
+    $matchTotals = getMatchTotals($matchData);
+
+    foreach ($matchData['participants'] as $participant)
+    {
+        $participantData = getPlayerData($matchData, $participant['participantId']);      
+        if ($participantData['summonerName'] == $targetSummonerName) /* this stuff filters */
+        {  
+            $dataParticipant['summonerName'] = $participantData['summonerName'];
+
+            foreach ($config['recordStats'] as $statName)
+            {
+                $dataParticipant[$statName] = $participant['stats'][$statName];
+            }
+
+            $matchParticipantData[$participant['teamId']][] = $dataParticipant;
+        }
+    }
+
+    echo '<hr><hr>';
+    var_dump($matchTotals);    
+    var_dump($matchParticipantData);    
+}
 
 
-var_dump($config);
-echo '<hr>';
+function getPlayerData($matchData, $participantId)
+{
+    foreach ($matchData['participantIdentities'] as $participantIdentity)
+    {
+        if ($participantIdentity['participantId'] == $participantId)
+        {
+            return $participantIdentity['player'];
+        }
+    }
 
-var_dump($result);
-echo '<hr>';
+    return false;
+}
 
-// wow
-echo 'ye';
+function getMatchTotals($matchData)
+{
+    global $config;     
 
+    $totals = array();
 
+    foreach ($matchData['participants'] as $participant)
+    {
+        foreach ($config['recordStats'] as $statName)
+        {
+            if (empty($totals[$participant['teamId']][$statName]))
+            {
+                $totals[$participant['teamId']][$statName] = 0;
+            }
+            
+            $totals[$participant['teamId']][$statName] += $participant['stats'][$statName];
+        }
+    }
 
+    return $totals;
+}
 
 function apiCall($endpoint)
 {
@@ -47,12 +101,13 @@ function apiCall($endpoint)
 	$response = curl_exec($curl);
 	$err = curl_error($curl);
 	curl_close($curl);    
+
     if ($err) 
     {
         return "cURL Error #:" . $err;
     } 
     else 
     {
-        return json_decode($response);
+        return json_decode($response, true);
     }
 }
